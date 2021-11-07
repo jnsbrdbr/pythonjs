48cb5315e6e24d0f6875917f6ec6bb64ce8d8b34
jnsbrdbr <jinu.b@liv.com>
2021-11-07 17:53:27 +0330

    first

---
+++ warmup.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plot
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+
+def warmup():
+    a=np.eye(5)
+    print(a)
+    return a
+
+warmup()
